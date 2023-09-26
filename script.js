067cf682499519147c7598bae1016421a1968e2b
shivani-5548 <90037384+shivani-5548@users.noreply.github.com>
2023-09-26 17:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+
+	let count=0;
+      library.forEach((i)=>{
+       if(i.readingStatus===true)
+       {
+         count++;
+       }
+      })
+      return count;
 };
 
 // Do not change the code below
